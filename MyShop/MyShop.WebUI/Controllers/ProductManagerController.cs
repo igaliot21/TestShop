270838e81f63076f45d3aa41de51ef86493d3dab
josemaria.izquierdo <josemaria.izquierdo@fhios.es>
2020-01-21 16:26:48 +0100

    Add image to the products registers

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,13 +35,17 @@ namespace MyShop.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
                 return View(product);
             }
             else {
+                if (file != null){
+                    product.Image = product.ID + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -65,7 +70,7 @@ namespace MyShop.WebUI.Controllers
 
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string ID)
+        public ActionResult Edit(Product product, string ID, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(ID);
             if (productToEdit == null)
@@ -80,9 +85,14 @@ namespace MyShop.WebUI.Controllers
                 } 
                 else
                 {
+                    if (file != null)
+                    {
+                        productToEdit.Image = product.ID + Path.GetExtension(file.FileName);
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+                    }
                     productToEdit.Category      = product.Category;
                     productToEdit.Description   = product.Description;
-                    productToEdit.Image         = product.Image;
+                    //productToEdit.Image         = product.Image;
                     productToEdit.Name          = product.Name;
                     productToEdit.Price         = product.Price;
 
