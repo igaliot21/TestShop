e127c70b88692e86753d8c779233390934ce2842
josemaria.izquierdo <josemaria.izquierdo@fhios.es>
2020-01-21 11:27:33 +0100

    Implement generic repository

---
+++ MyShop/MyShop.Core/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core
+{
+    public abstract class BaseEntity
+    {
+        public string ID { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public BaseEntity()
+        {
+            this.ID = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+        }
+    }
+}
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -11,15 +11,15 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
         public ProductManagerController()
         {
-            context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             List<Product> products = context.Collection().ToList();
             return View(products);
